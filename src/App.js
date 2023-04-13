157ef7921e44803c6903210f651c4490e08128e1
MysteryFlask <76215428+MysteryFlask@users.noreply.github.com>
2023-04-13 12:15:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import Iframe from 'react-iframe';
-import Draggable from 'react-draggable';
-import { ResizableBox } from 'react-resizable';
-import './style.css';
-import 'react-resizable/css/styles.css'
+import React, { useState, useEffect } from "react";
+import Iframe from "react-iframe";
+import Draggable from "react-draggable";
+import { ResizableBox } from "react-resizable";
+import "./style.css";
+import "react-resizable/css/styles.css";
 
 function App() {
   const [frames, setFrames] = useState([]);
 
   useEffect(() => {
-    const storedFrames = JSON.parse(localStorage.getItem('frames')) || [];
+    const storedFrames = JSON.parse(localStorage.getItem("frames")) || [];
     setFrames(storedFrames);
   }, []);
 
   const addFrame = () => {
-    const url = prompt('Enter the URL of the website:');
+    const url = prompt("Enter the URL of the website:");
     if (url) {
       const newFrame = { url };
       setFrames([...frames, newFrame]);
-      localStorage.setItem('frames', JSON.stringify([...frames, newFrame]));
+      localStorage.setItem("frames", JSON.stringify([...frames, newFrame]));
     }
   };
 
   const clearFrames = () => {
     setFrames([]);
-    localStorage.removeItem('frames');
+    localStorage.removeItem("frames");
   };
 
   const removeFrame = (frame) => {
-	const index = frames.indexOf(frame);
-	console.log(index)
-	const updatedFrames = [...frames.slice(0, index), ...frames.slice(index + 1)];
-	console.log(updatedFrames)
-	setFrames(updatedFrames);
-	localStorage.setItem('frames', JSON.stringify(updatedFrames));
+    const index = frames.indexOf(frame);
+    console.log(index);
+    const updatedFrames = [
+      ...frames.slice(0, index),
+      ...frames.slice(index + 1)
+    ];
+    console.log(updatedFrames);
+    setFrames(updatedFrames);
+    localStorage.setItem("frames", JSON.stringify(updatedFrames));
   };
 
-
   return (
-    <div>
+    <>
       <div className="input-container">
         <button onClick={addFrame}>Add Frame</button>
         <button onClick={clearFrames}>Clear Frames</button>
       </div>
-      <div className="frame-container">
-        {frames.map((frame, index) => (
-	  <ResizableBox
-	     	minConstraints={[100, 100]}
-	     	draggableOpts={{ handle: ".drag-handle" }}
-	      >
-	      	<div className="drag-handle"></div>
-	    <button className="close-button" onClick={() => removeFrame(frame)}>X</button>
-	    <Iframe url={frame.url} />
-	  </ResizableBox>
-        ))}
-      </div>
-    </div>
+
+      {frames.map((frame, index) => (
+        <Draggable key={index} handle={`#drag-handle-${index}`}>
+          <ResizableBox
+            width={200}
+            height={200}
+            minConstraints={[100, 100]}
+            maxConstraints={[300, 300]}
+          >
+            <div className="resize-drag-content">
+              <div className="drag-handle" id={`.drag-handle-${index}`}></div>
+              <button
+                className="close-button"
+                onClick={() => removeFrame(frame)}
+              >
+                X
+              </button>
+              <Iframe className="iframe" url={frame.url} />
+            </div>
+          </ResizableBox>
+        </Draggable>
+      ))}
+    </>
   );
 }
 
